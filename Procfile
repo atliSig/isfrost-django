3e15d6a4842010b4077fea9778b41b9468dd0c09
buttlet <atlisigurgeirsson@gmail.com>
2017-12-08 22:13:32 +0100

    changes to procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn isfrost_project.wsgi --log-file -
+web: gunicorn isfrost_project.wsgi --pythonpath isfrost_project --log-file -
--- isfrost_project/settings/production.py
+++ isfrost_project/settings/production.py
@@ -8,7 +8,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
-
 SECRET_KEY = config('SECRET_KEY')
 
 ALLOWED_HOSTS = ['isfrost-django.herokuapp.com']
